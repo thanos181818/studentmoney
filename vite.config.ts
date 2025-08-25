f2c4d8f1cee731d847a34d9fe9778850967edf24
thanos181818 <sahil.mehta23@spit.ac.in>
2025-08-26 01:08:18 +0530

    Fix Vite config loading error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: '/budgetbuddy/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
